8fc4abdca5208e0c28ab6b594738f143eabeda61
Utkarsh Agarwal <162401935+utkarshagarwal03@users.noreply.github.com>
2024-09-18 22:51:27 +0530

    Create Sum_naturalNo.c

---
+++ Sum_naturalNo.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,i=0;
+    printf("sum of first  natural no. is\n");
+    scanf("%d",&n);
+    for (n;n>0;n--)
+    {
+        i=i+n;
+    
+    }
+    printf("%d",i);
+    return 0;
+}
